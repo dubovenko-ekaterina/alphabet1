3f570a7beed128fbdd23cf5cf882b07dbeaf7629
Kate <dubovenko-ekaterina@mail.ru>
2021-10-19 09:42:39 +0300

    added classes for screen buffer

--- alphabet1/main.cpp
+++ alphabet1/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
+struct symbol
+{
+private:
+    array<string, 6> graphics;
+public:
+    symbol(string i1, string i2, string i3, string i4, string i5, string i6)
+    {
+        graphics[0] = i1;
+        graphics[1] = i2;
+        graphics[2] = i3;
+        graphics[3] = i4;
+        graphics[4] = i5;
+        graphics[5] = i6;
+    }
+    string getline(int i)
+    {
+        return graphics[i];
+    }
+};
+
+struct screen
+{
+private:
+    array<string, 6> graphics;
+public:
+    void add(symbol toadd)
+    {
+        for (int i(0); i<6; i++)
+            graphics[i]+=toadd.getline(i);
+    }
+    void print()
+    {
+    for (auto i: graphics)
+        cout << i << endl;
+    }
+};
+
+screen mainscreen;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    mainscreen.print();
     return 0;
 }
+
